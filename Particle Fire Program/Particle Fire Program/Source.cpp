0824d54da89edbd6ca774fae70f9e536f4c8a922
sethpow <sethmadspowelson@gmail.com>
2020-10-31 11:35:16 -0700

    SDL Window; 800x600

--- Particle Fire Program/Particle Fire Program/Source.cpp
+++ Particle Fire Program/Particle Fire Program/Source.cpp
@@ -3,12 +3,44 @@
 using namespace std;
 
 int main(int argc, char* args[]) {
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL Init Video failure." << endl;
 		return 1;
 	}
 
-	cout << "SDL Init Video Running." << endl;
+	SDL_Window* window = SDL_CreateWindow("Particle Fire",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	if (window == NULL) {
+		SDL_Quit();
+		cout << SDL_GetError() << endl;
+		return 2;
+	}
+
+	bool quit = false;
+	SDL_Event event; // needs address so SDL can fill in info for events that occur since previous call
+	// Game/Event loop
+	while (!quit) {
+		// Update particles
+		// Draw particles
+		// Check for events
+
+		// Process event queues
+		// While there is a new event is waiting to be processed, will remain True
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+
+
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
 	return 0;
 }

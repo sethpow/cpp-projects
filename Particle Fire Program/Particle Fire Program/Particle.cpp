588894e9ad980a23114dd6adb1b063c0f5b11250
sethpow <sethmadspowelson@gmail.com>
2020-11-03 16:43:13 -0800

    explosion rotation

--- Particle Fire Program/Particle Fire Program/Particle.cpp
+++ Particle Fire Program/Particle Fire Program/Particle.cpp
@@ -5,8 +5,15 @@
 namespace particlefire {
 
 	Particle::Particle(): m_x(0), m_y(0) {
+		init();
+	}
+
+	void Particle::init() {
+		m_x = 0;
+		m_y = 0;
 		m_direction = (2 * 3.14159265359 * rand()) / RAND_MAX;
-		m_speed = (0.005 * rand()) / RAND_MAX;
+		m_speed = (0.05 * rand()) / RAND_MAX;
+		m_speed *= m_speed;
 	}
 
 	Particle::~Particle() {
@@ -15,11 +22,22 @@ namespace particlefire {
 
 	// update position of particles
 	void Particle::update() {
+		// add particle curling
+		m_direction += m_direction * .0005;
+
 		double xspeed = m_speed * cos(m_direction);
 		double yspeed = m_speed * sin(m_direction);
 
 		m_x += xspeed;
 		m_y += yspeed;
+
+		if (m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1) {
+			init();
+		}
+
+		if (rand() < RAND_MAX / 100) {
+			init();
+		}
 	}
 
 }
--- Particle Fire Program/Particle Fire Program/Particle.h
+++ Particle Fire Program/Particle Fire Program/Particle.h
@@ -7,8 +7,13 @@ namespace particlefire {
 struct Particle {
 	double m_x;
 	double m_y;
+
+private:
 	double m_speed;
 	double m_direction;
+
+private:
+	void init();
 
 public:
 	Particle();
